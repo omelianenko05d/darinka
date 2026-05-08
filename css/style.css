1d4e3f32b525754bec4b5cfac6e9d6703bd2f493
omelianenko05d <OmeDar23@student.skolahostivar.cz>
2026-05-08 22:46:14 +0200

    Update style.css

--- css/style.css
+++ css/style.css
@@ -1,4 +1,4 @@
-/* Student si tu nastavil zakladni barvy celeho webu */
+
 /* ===== GLOBAL STYLES — LUMIS MUSEUM ===== */
 
 @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400&family=Jost:wght@300;400;500&display=swap');
